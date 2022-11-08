20652283542b56b8bdbffcfc895f34e048548930
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-11-08 16:24:21 +0100

    secont commit

--- index.js
+++ index.js
@@ -10,20 +10,29 @@ fetch("https://api.pexels.com/v1/search?query=Ocean", options)
   .then((data) => {
     // console.log(data.photos[0].src.original);
     const loadbtn = document.querySelector("#load-image");
+    const cards = document.querySelectorAll(".card");
+
     const imageSection = document.querySelectorAll(".card-img-top");
     loadbtn.addEventListener("click", () => {
       const photos = data.photos;
       for (let p = 0; p < photos.length; p++) {
         const imageDisplayerContainer = document.createElement("div");
+        imageDisplayerContainer.classList.add("card-img-top");
         const imageDisplayer = document.createElement("img");
-        imageDisplayer.setAttribute("src", photos[p].src.original);
+        imageDisplayer.src = photos[p].src.original;
         imageDisplayerContainer.appendChild(imageDisplayer);
+        console.log(photos[p].src.original);
 
         for (let image of imageSection) {
-          image.innerHTML = imageDisplayerContainer;
-          image.appendChild(imageDisplayerContainer);
+          image.remove();
+
+          //   image.innerHTML = imageDisplayerContainer;
+          //   image.appendChild(imageDisplayerContainer);
         }
-        // console.log(photo.photos);
+        for (let card of cards) {
+          card.appendChild(imageDisplayerContainer);
+          // imageDisplayerContainer.classList.add("card-img-top");
+        }
       }
     });
   });
